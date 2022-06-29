e68b318975ad4dcf82512fa70b6c9869ae870aa9
ahmed-Elnewery <ahmed.elnewery@boostiny.com>
2022-06-29 13:33:14 +0200

    learnign  vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,42 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
+Vue.config.devtools = true
+
 const store = new Vuex.Store({
     state:{
-        counter:0
+        counter:0,
+        movies:[]
     },
     getters:{
         getTripleNumber(state){
             return state.counter *3
         }
     },
-    mutations:{},
-    actions:{}
+    mutations:{
+        increment(state){
+            state.counter +=1
+        },
+        incrementByNumber(state,payload){
+            state.counter+=payload.by   
+        },
+        getAllMovies(state,payload){
+            state.movies= payload
+        }
+    },
+    actions:{
+        incrementByOne(context){
+            context.commit('increment')
+        },
+        incrementByPayload(context,payload ){
+            context.commit('incrementByNumber',payload)
+        },
+         getAllMovies(context){
+            axios.get("http://localhost:3000/movies/").then((res)=>{
+                context.commit('getAllMovies',res.data)
+            })
+        }
+    }
 })
 export default store
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,15 +1,53 @@
 <script>
+import { mapActions } from "vuex";
+
 export default {
   name: "TestPage",
   computed: {
     counter() {
       return this.$store.state.counter;
     },
+    tripple() {
+      return this.$store.getters.getTripleNumber;
+    },
+    movies() {
+      return this.$store.state.movies;
+    },
+  },
+  methods: {
+    ...mapActions(["getAllMovies"]),
+    increment() {
+      this.$store.dispatch("incrementByOne");
+    },
+    incrementByTen() {
+      this.$store.dispatch("incrementByPayload", { by: 10 });
+    },
+  },
+  created() {
+    this.getAllMovies();
   },
 };
 </script>
 <template>
-  <div>test {{ counter }}</div>
+  <div>
+    <div>counter {{ counter }}</div>
+    <div>tripple {{ tripple }}</div>
+    <div>
+      <button @click="increment">increment</button>
+      <button @click="incrementByTen">increment by 10</button>
+    </div>
+
+    <hr />
+    <div class="mt-8">
+      <div
+        class="px-4 py-3 bg-red-200 mb-2"
+        v-for="movie in movies"
+        :key="movie.id"
+      >
+        {{ movie.title }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <style></style>

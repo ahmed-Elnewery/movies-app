2c421f7aa2496945b5717224524bd5de308fff41
ahmed-Elnewery <ahmed.elnewery@boostiny.com>
2022-07-03 08:58:40 +0200

    star icon | style movie card

--- src/components/Movies/MovieCard.vue
+++ src/components/Movies/MovieCard.vue
@@ -1,5 +1,7 @@
 <script>
+import StarIcon from "../UI/SVG/StarIcon.vue";
 export default {
+  components: { StarIcon },
   props: {
     movie: {
       type: Object,
@@ -11,7 +13,7 @@ export default {
 <template>
   <router-link :to="'/movies/' + movie.id">
     <div
-      class="border rounded-md relative overflow-hidden shadow-lg bg-white transform transition duration-300 hover:scale-105"
+      class="border rounded-md relative overflow-hidden shadow-lg bg-gray-50 transform transition duration-300 hover:scale-105"
     >
       <div class="flex">
         <img :src="movie.image" />
@@ -24,9 +26,11 @@ export default {
               class="px-2 py-1 bg-sky-400 text-white rounded-full text-xs font-bold"
               >{{ movie.year }}</span
             >
-            <div class="flex items-center gap-x-1">
-              <img width="20" src="../../assets/SVG/star-solid.svg" />
-              <span>{{ movie.imDbRating }}</span>
+            <div class="flex items-center gap-x-2">
+              <StarIcon />
+              <span class="text-sm font-semibold text-yellow-500">{{
+                movie.imDbRating
+              }}</span>
             </div>
           </div>
           <div class="text-sm text-gray-400">
